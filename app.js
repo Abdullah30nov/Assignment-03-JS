ec7fac04f953ef99496218a6a92aded02c9f5b6d
Abdullah30nov <abdullahbinaslam64@gmail.comgit config --global user.email abdullahbinaslam64@gmail.com>
2024-03-03 04:12:51 -0800

    Chapter 9,10,11,12

---
+++ app.js
@@ -0,0 +1,119 @@
+//                     //  QUESTION-NO-01
+// var city = (prompt("Enter city name"))
+// if (city === "karachi"){
+//     alert("Welcome To Tha City Of Light")
+// }
+// else if (city >= "lahore"){
+//     alert("Welcome To Tha Gardens Of The Mughals")
+// }
+// else if(city === "islamabad"){
+//     alert("Welcome To The City Of Peace")
+// }
+// else {
+//     alert("Not Sure");
+// }
+//                     //  QUESTION-NO-02
+// var gender = (prompt("What's your gender"))
+// if (gender === "male"){
+//     alert("Good Morning Sir")
+// }
+// else if (gender >= "female"){
+//     alert("Good Morning Ma'am")
+// }
+// else {
+//     alert("Not Sure");
+// }
+//                     //  QUESTION-NO-03
+// var lightcolor = (prompt("Enter traffic light colours"))
+// if (lightcolor === "red"){
+//     alert("Must stop")
+// }
+// else if (lightcolor >= "yellow"){
+//     alert("Ready to move")
+// }
+// else if(lightcolor === "green"){
+//     alert("Move now")
+// }
+// else {
+//     alert("Not Sure");
+// }
+//                     //  QUESTION-NO-04
+// var fuel = +(+prompt("Remainig fuel in your car (in Litres)"))
+// if (fuel >= 30 ){
+//     alert("enough to Drive")
+// }
+// else if (fuel <= 30){
+//     alert("“Please refill the fuel in your car”")
+// }
+                //    QUESTION-NO-06
+// var eng = +(prompt("Enter English marks"));
+// var urdu = +(prompt("Enter Urdu marks"));
+// var math = +(prompt("Enter Math marks"));
+
+// var marks = eng + urdu + math;
+// var total = (eng + urdu + math) / 300 * 100 ;7887
+//  if  (total >= 80 && 100){
+//     document.write("<h1>Marks Sheet</h1>  <br>"+"Total marks: 300 <br>" + "Obtainerd marks: " + marks + "<br>" + "Your Percentage is: " + total + " <br> " + "Your Grade is: A-ONE" + "<br> " + "Remarks: Excellent")
+// }
+// else if (total >= 70 && 80){
+//     document.write("<h1>Marks Sheet</h1>  <br>"+"Total marks: 300 <br>" + "Obtainerd marks: " + marks + "<br>" + "Your Percentage is: " + total + " <br> " + "Your Grade is: A" + "<br> " + "Remarks: Good")
+// }
+// else if (total >= 60 && 70){
+//     document.write("<h1>Marks Sheet</h1>  <br>"+"Total marks: 300 <br>" + "Obtainerd marks: " + marks + "<br>" + "Your Percentage is: " + total + " <br> " + "Your Grade is: B" + "<br> " + "Remarks: Need to improve")
+// }
+// else if (total <= 60){
+//     document.write("<h1>Marks Sheet</h1>  <br>"+"Total marks: 300 <br>" + "Obtainerd marks: " + marks + "<br>" + "Your Percentage is: " + total + " <br> " + "You are Fail" + "<br> " + "Remarks: Sorry")
+// }
+// else{
+//     alert("Not Sure")
+// }
+                        //    QUESTION-NO-07
+// var number = +prompt("Guess the secret number");
+// var secret = 5
+// if (number === 5){
+//     alert("'Bingo! Correct answer'");
+// }
+// else if(number !== 5) {
+//     alert("Close enough to the correct answer");
+// }
+                                    //    QUESTION-NO-08
+// var a = +prompt("Enter the number");
+// var a1 = a/3
+// if (a >= a1){
+//     alert("If the number is divisible by 3");
+// }
+                                    //  QUESTION-NO-09
+                                    //  QUESTION-NO-10
+// var temp = +prompt("Enter a Temperature");
+// if(temp >= 40){
+//     alert("It's too hot outside.");
+// }
+// else if(temp > 30){
+//     alert("The Weather today is normal.");
+// };
+// if(temp > 20){
+//     alert("Today's Weather is cool.");
+// };
+// if(temp > 10){
+//     alert("OMG! Today's weather is so cool.");
+// };
+                            // QUESTION-NO-11
+// var number = prompt("Enter the number");
+// if (number % 2 == 0){
+//     alert("This is Even number");
+// }
+// else{
+//     alert("This is ODD number");
+// }
+                                    //  QUESTION-NO-11
+// var num1 = +prompt("Enter first number");
+// var num2 = +prompt("Enter last number");
+// var add = num1 + num2, sub = num1-num2, multi = num1*num2, division = num1/num2, reminder = num1%num2;
+// alert("Addition: " + add + "\n" + "Subtraction: " +  sub + "\n" +"Multiplication: " +  multi + "\n" +"Division: " +  division + "\n" +"Reminder: " +  reminder); 
+
+                            // CHAPTER-NO-12/13
+                            // QUESTION-NO-01
+var a = prompt("Enter any thing");
+if (a === is,a,number){
+    alert("This is a number")
+}
